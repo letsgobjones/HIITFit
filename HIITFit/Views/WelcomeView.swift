cef3bf0ce9632ef20bd66f950418c16b184dc1ad
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-01-30 01:15:02 +0900

    Set up paging tab view

--- HIITFit/Views/WelcomeView.swift
+++ HIITFit/Views/WelcomeView.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2023 Kodeco LLC
-///
+/// Copyright (c) 2024 Kodeco LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,24 +32,12 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  var body: some View {
-    TabView {
-      Text("Welcome")
-        .tabItem { Text("Welcome") }
-      Text("Exercise 1")
-        .tabItem { Text("Exercise 1") }
-      Text("Exercise 2")
-        .tabItem { Text("Exercise 2") }
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-
-  }
 }
 
-
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+#Preview {
+    WelcomeView()
 }
